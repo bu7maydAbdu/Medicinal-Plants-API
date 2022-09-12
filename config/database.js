4df65be752c4fc1901a6cca9f4bf37037278a342
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-09-12 04:36:14 +0300

    string

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_STRING_2, {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true
    
