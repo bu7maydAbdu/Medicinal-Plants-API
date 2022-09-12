4e3d9ea363dd40422f371fb04c1b18c09886f667
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-09-12 04:23:37 +0300

    adding

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -5,7 +5,7 @@ const mainController = require("../controllers/mainController")
 
 router.get("/", mainController.getHomePage)
 
-router.get(process.env.adminLoginPagePath, mainController.adminLoginPage)
+router.get(`/${process.env.adminLoginPagePath}`, mainController.adminLoginPage)
 
 router.post("/adminLoggingIn", mainController.adminLoggingIn)
 
