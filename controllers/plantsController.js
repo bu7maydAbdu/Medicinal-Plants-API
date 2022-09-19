47a5a4271438e81e501d7737e742cf31d6b87094
mohammed abdulrahman idrees <71595830+bu7maydAbdu@users.noreply.github.com>
2022-09-19 20:56:18 +0300

    api key

--- controllers/plantsController.js
+++ controllers/plantsController.js
@@ -2,21 +2,7 @@ const cloudinary = require("../middleware/cloudinary");
 const Plant = require("../models/Plant")
 
 
-// app.post('/addPlant', (request, response) => {
-//     db.collection('plants').insertOne({plantName: request.body.plantName, plantArabicName: request.body.plantArabicName, familyName: request.body.familyName, scientificName: request.body.scientificName,
-//     mainIngredient: request.body.mainIngredient,partUsed: request.body.partUsed, areaOfPlant: request.body.area, biologicalActivity: request.body.biologicalActivity, plantImage: request.body.plantImage})
-//     .then(result => {
-//         console.log('plant Added')
-//         response.redirect('/')
-//     })
-//     .catch(error => console.error(error))
-// })
 
-//            }catch(err){
-//              console.error(err)
-//            }
-    
-//     }
 
 module.exports = {
     getApi : async (request, response)=>{
@@ -39,7 +25,7 @@ module.exports = {
 
         try {
 
-            // const result = await cloudinary.uploader.upload(request.file.path);
+            const result = await cloudinary.uploader.upload(request.file.path);
 
             await Plant.create({
                 
@@ -49,8 +35,8 @@ module.exports = {
                      mainIngredient: request.body.mainIngredient,
                      partUsed: request.body.partUsed, areaOfPlant: request.body.area,
                       biologicalActivity: request.body.biologicalActivity,
-                        //  image: result.secure_url,
-                    //  cloudinaryId: result.public_id
+                         image: result.secure_url,
+                     cloudinaryId: result.public_id
     
                     })
 
